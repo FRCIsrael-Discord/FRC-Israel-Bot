e65aa2493d1d4acd60b42a43a8a54cae0937d4c2
Yarondr <yarondrukh@gmail.com>
2022-05-18 02:32:24 +0300

    Removed unused event

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import dotenv from 'dotenv';
 import path from 'path';
 dotenv.config();
 
+const myTestServerId = '851789251754328064';
+const FRCIsraelId = '959144521621458974'
+
 const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
@@ -19,35 +22,10 @@ client.on('ready', () => {
         commandDir: path.join(__dirname, 'commands'),
         typeScript: true,
 
-        testServers: ['851789251754328064', '959144521621458974']
+        testServers: [myTestServerId, FRCIsraelId]
     });
 
     console.log("FRC Israel bot is now active!");
-});
-
-client.on('guildMemberUpdate', (oldMember, newMember) => {
-    // const server = newMember.guild;
-    // console.log(oldMember.nickname, newMember.nickname);
-    // if (newMember.nickname && newMember.nickname != oldMember.nickname) {
-    //     if (/\w+ \| \d/g.test(newMember.nickname)) {
-    //         console.log("changed nick");
-
-    //         const number = newMember.nickname.split("|")[1].replace(/[^0-9]/g, "");
-
-    //         if (teamList.includes(number)) {
-    //             teamList.forEach(team => {
-    //                 const role = server.roles.cache.find(role => role.name.split(" | ")[1] == team);
-    //                 if (role != undefined) newMember.roles.remove(role);
-    //             });
-
-    //             const newRole = server.roles.cache.find(role => role.name.split(" | ")[1] == number);
-    //             if (newRole != undefined) newMember.roles.add(newRole);
-    //         } else newMember.setNickname(oldMember.nickname);
-    //     } else {
-    //         if (/\w+ \| \d/g.test(oldMember.nickname!)) newMember.setNickname(oldMember.nickname);
-    //         else newMember.setNickname("");
-    //     }
-    // }
 });
 
 client.login(process.env.TOKEN);
