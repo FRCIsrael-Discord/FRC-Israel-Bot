b39e15e3f93c6bcc3f563db177752ccc3e1faa37
Yarondr <yarondrukh@gmail.com>
2024-01-22 19:05:48 +0200

    Add title and question to post type

--- src/lib/types/support/post.ts
+++ src/lib/types/support/post.ts
@@ -2,6 +2,8 @@ import { SupportType } from "./supportTypes";
 
 export interface SupportPost {
     channelId: string;
+    title: string;
+    question: string;
     type: SupportType;
     authorId: string;
     approved: boolean;
