4008afe5ca7c039fe42bdca088528584c2db5354
Yarondr <yarondrukh@gmail.com>
2024-01-22 18:55:46 +0200

    Change slash commands folder name

--- src/handlers/slashCommandsHandler.ts
+++ src/handlers/slashCommandsHandler.ts
@@ -7,15 +7,15 @@ import { logInfo } from "../utils/logger";
 export function loadSlashCommands(bot: Bot, reload: boolean) {
     const { slashCommands } = bot;
 
-    const commandsPath = path.join(__dirname, "../slashCommands");
+    const commandsPath = path.join(__dirname, "../commands");
     fs.readdirSync(commandsPath).forEach((category: string) => {
         const commandPath = path.join(commandsPath, category);
         let slashCommandsFiles = getFiles(commandPath, ".ts");
 
         slashCommandsFiles.forEach((f) => {
             if (reload)
-                delete require.cache[require.resolve(`../slashCommands/${category}/${f}`)]
-            const command: SlashCommand = require(`../slashCommands/${category}/${f}`)
+                delete require.cache[require.resolve(`../commands/${category}/${f}`)]
+            const command: SlashCommand = require(`../commands/${category}/${f}`)
             slashCommands.set(command.name, command)
         })
     })
