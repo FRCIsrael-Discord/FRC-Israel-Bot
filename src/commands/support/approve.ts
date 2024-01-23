92524e6f748862cd5f59e1618e914383718e380b
Yarondr <yarondrukh@gmail.com>
2024-01-23 17:34:33 +0200

    Send message to author on approve

--- src/commands/support/approve.ts
+++ src/commands/support/approve.ts
@@ -39,6 +39,7 @@ module.exports = {
 
         const firstMessage = await channel.fetchStarterMessage();
         firstMessage!.reply({ content: `<@&${supportRole}>\nשאלה זאת אושרה על ידי הצוות!` });
+        firstMessage!.author.send({ content: `**השאלה שלך בנושא "${post.title}" אושרה על ידי הצוות!**\nהאנשים המתאימים תוייגו ויענו לך בהמשך.\nניתן לצפות בשאלה כאן:\n<#${channel.id}>` });
 
         await approvePost(post);
         await interaction.editReply({ content: 'Approved!' });
